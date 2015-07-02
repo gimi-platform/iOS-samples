2d3ceed732c81edcec9ed7c65ba2c9ec155ade7c
Hoang Nguyen Ngoc <hoangnn@toivui.com>
2015-07-02 17:20:25 +0700

    initial commit

---
+++ Sample/Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by Hoang Nguyen Ngoc on 7/2/15.
+//  Copyright (c) 2015 Gimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample/Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by Hoang Nguyen Ngoc on 7/2/15.
+//  Copyright (c) 2015 Gimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
